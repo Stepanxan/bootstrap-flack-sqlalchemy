32a8db77a2f1474e8dc4592641c2e2c3dca0af5e
Stepanxan <stepanxan@gmail.com>
2022-11-29 21:44:47 +0200

    first

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ class Departments(db.Model):
     locationID = db.Column(db.String(200), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
+
     def __repr__(self):
         return f"<departments {self.id}>"
 
@@ -24,10 +25,10 @@ class Employees(db.Model):
     employeeID = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(200), nullable=False)
     last_name = db.Column(db.String(200), nullable=False)
-    email = db.Column (db.String(200), nullable=False)
-    phone_number = db.Column (db.Integer, nullable=False)
+    email = db.Column(db.String(200), nullable=False)
     salary = db.Column(db.Integer, nullable=False)
-    departmentID = db.Column(db.Integer, db.ForeignKey('departmentID'))
+    departmentID = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"<employees {self.id}>"
@@ -36,6 +37,94 @@ class Employees(db.Model):
 @app.route('/home')
 def index():
     return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/selectdepartments')
+def select_departments():
+    departments = Departments.query.order_by(Departments.date.desc()).all()
+    return render_template("select-departments.html", departments=departments)
+
+
+@app.route('/selectdepartments/<int:departmentID>')
+def select_detail(departmentID):
+    department = Departments.query.get(departmentID)
+    return render_template("select_detail.html", department=department)
+
+
+@app.route('/selectdepartments/<int:departmentID>/deletedepartments')
+def select_delete_department(departmentID):
+    department = Departments.query.get_or_404(departmentID)
+
+    try:
+        db.session.delete(department)
+        db.session.commit()
+        return redirect('/selectdepartments')
+    except:
+        return "При видаленні даних сталась помилка"
+
+
+@app.route('/selectdepartments/<int:departmentID>/updatedepartments', methods=['POST', 'GET'])
+def update_departments(departmentID):
+    department = Departments.query.get(departmentID)
+    if request.method == "POST":
+        department.departmentNAME = request.form['departmentNAME']
+        department.managerID = request.form['managerID']
+        department.locationID = request.form['locationID']
+
+        try:
+            db.session.commit()
+            return redirect('/selectdepartments')
+        except:
+            return "При заповненні вийшла помилка"
+    else:
+        return render_template("update-departments.html", department=department)
+
+@app.route('/selectemployees')
+def select_employees():
+    employees = Employees.query.order_by(Employees.date.desc()).all()
+    return render_template("select_employees.html", employees=employees)
+
+
+@app.route('/selectemployees/<int:employeeID>')
+def select_detailemp(employeeID):
+    employee = Employees.query.get(employeeID)
+    return render_template("select_detailemp.html", employee=employee)
+
+
+@app.route('/selectemployees/<int:employeeID>/deleteemployees')
+def select_delete_employees(employeeID):
+    employee = Employees.query.get_or_404(employeeID)
+
+    try:
+        db.session.delete(employee)
+        db.session.commit()
+        return redirect('/selectemployees')
+    except:
+        return "При видаленні даних сталась помилка"
+
+
+@app.route('/selectemployees/<int:employeeID>/updateemployees', methods=['POST', 'GET'])
+def update_employees(employeeID):
+    employee = Employees.query.get(employeeID)
+    if request.method == "POST":
+        employee.first_name = request.form['first_name']
+        employee.last_name = request.form['last_name']
+        employee.email = request.form['email']
+        employee.salary = request.form['salary']
+        employee.departmentID = request.form['departmentID']
+
+        try:
+            db.session.commit()
+            return redirect('/selectemployees')
+        except:
+            return "При заповненні вийшла помилка"
+    else:
+        return render_template("update-employees.html", employee=employee)
 
 
 @app.route('/create-departments', methods=['POST', 'GET'])
@@ -52,12 +141,34 @@ def create_departments():
             db.session.commit()
             return redirect('/')
         except:
-            return "При заповненні виникла помилка"
-
+            return "При заповненні вийшла помилка"
     else:
         return render_template("create-departments.html")
 
 
+@app.route('/create-employees', methods=['POST', 'GET'])
+def create_employees():
+    if request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        salary = request.form['salary']
+        departmentID = request.form['departmentID']
+
+        employees = Employees(first_name=first_name, last_name=last_name, email=email, salary=salary, departmentID=departmentID)
+
+        try:
+            db.session.add(employees)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "При заповненні вийшла помилка"
+    else:
+        return render_template("create-employees.html")
+
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
